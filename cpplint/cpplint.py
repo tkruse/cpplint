1942263ad56a93a711bee5e76b5b0eefeefaad1d
Thibault Kruse <kruset@in.tum.de>
2012-11-23 23:54:47 +0100

    code style

--- cpplint/cpplint.py
+++ cpplint/cpplint.py
@@ -1029,7 +1029,8 @@ def CheckForCopyright(filename, lines, error):
   # We'll say it should occur by line 10. Don't forget there's a
   # dummy line at the front.
   for line in range(1, min(len(lines), 11)):
-    if re.search(r'Copyright', lines[line], re.I): break
+    if re.search(r'Copyright', lines[line], re.I):
+      break
   else:                       # means no copyright line was found
     error(filename, 0, 'legal/copyright', 5,
           'No copyright message found.  '
